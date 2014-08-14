0944f5c2cc474276c1f61f8b3f12101f4e4ead42
Robert van 't Hof <robert.van.thof@mendix.com>
2014-08-14 11:31:32 +0200

    First release of the Bootstrap RTE widget on GitHub.

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-Bootstrap-RTE
+Basic Bootstrap Wysiwyg Editor
 =============
 
 Basic, bootstrap based, rich text editor.
+
+##Description
+This widget is a basic, more simplistic, more style-friendly version of the Simple Rich Text Editor. It has been created specifically for use in MX5 projects. It provides basic text styling functionality such as bold,italic andunderline. 
+
+The widget is based on the jQuery bootstrap wysiwyg plugin: http://mindmup.github.io/bootstrap-wysiwyg/
+
+The only configuration needed is the referenced text attribute.
+
+##Features and limitations
+
+The wysiwyg div accepts all shortcuts that are used in text editing programs such as MS Word.
+Not all buttons are available yet.
+
+##Dependencies
+
+Mendix 5.x Environment
+jQuery (added to the package)
+bootstrap-wysiwyg plugin (added to the package)
+
+##Configuration
+
+The widget should be placed inside a dataview.
+
+##Properties
+
+Data attribute: The attribute the content should be saved to.
+Input height: The height of the input field in px.
---
+++ src/BootstrapRTE/widget/BootstrapRTE.js
@@ -0,0 +1,316 @@
+dojo.require("BootstrapRTE.lib.jquery-183-min");
+dojo.require("BootstrapRTE.lib.bootstrap-wysiwyg");
+dojo.require("BootstrapRTE.lib.external.jquery-hotkeys");
+mxui.dom.addCss(dojo.moduleUrl("BootstrapRTE.lib", "font/css/font-awesome.css"));
+
+(function($) {
+
+dojo.declare("BootstrapRTE.widget.BootstrapRTE", mxui.widget._WidgetBase, {
+	inputargs : {
+		attribute : ""
+	},
+
+	//housekeeping
+	_mxObj : null,
+	_inputfield : null,
+	_bigBox : null,
+	_toolbarNode : null,
+	_handle : null,
+
+	postCreate : function(){
+		this._inputfield = dojo.create('div', { 'id' : this.id + '_editor' });
+		this.createToolbar();
+		this.addEditor();			
+		this.actLoaded();
+	},
+
+	update : function(obj, callback){
+		if (this._handle){
+			mx.data.unsubscribe(this._handle);
+		}
+		if(obj){
+			var self = this;
+			this._mxObj = obj;
+			//set the content on update
+			dojo.html.set(this._inputfield, this._mxObj.get(this.attribute));
+			// fix microflow change calls
+			this._handle = mx.data.subscribe({
+				guid : this._mxObj.getGuid(),
+				callback: function(){
+					dojo.html.set(self._inputfield, self._mxObj.get(self.attribute));
+				}
+			});	
+		}
+		callback && callback();
+	},
+
+	createToolbar : function(){
+		this._toolbarNode = dojo.create('div', { 'class' : 'btn-toolbar toolbar_' + this.id, 'data-role' : 'editor-toolbar-' + this.id , 'data-target' : '#' + this.id + '_editor' });
+		var self = this;
+		//Freedom to create our own toolbar. With freedom comes responsibility.
+		//Below we define all the buttons that we'll render.
+		var toolbarButtons = [
+			[
+				{toggle: 'font', icon: 'font',
+				fonts: [
+				{name : 'Arial'},
+				{name : 'Courier'},
+				{name : 'Helvetica'},
+				{name : 'Lucida Grande'},
+				{name : 'Times New Roman'},
+				{name : 'Verdana'}
+				]}
+			],
+			[
+				{toggle: 'fontsize', icon: 'text-height', 
+				sizes: [
+				{type: 'h1', name: 'Header 1'}, 
+				{type: 'h2', name: 'Header 2'},
+				{type: 'h3', name: 'Header 3'},
+				{type: 'p', name:'Normal'}
+				]}
+			],
+			[
+				{type: 'bold', 	icon: 'bold'}, 
+				{type: 'italic', icon: 'italic'}, 
+				{type: 'underline', icon: 'underline'}, 
+				{type: 'strikethrough', icon: 'strikethrough'}
+			],
+			[
+				{type: 'insertunorderedlist', icon: 'list-ul'}, 
+				{type: 'insertorderedlist', icon: 'list-ol'}, 
+				{type: 'outdent', icon: 'indent-left'}, 
+				{type: 'indent', icon: 'indent-right'}
+			], 
+			[
+				{type: 'justifyleft', icon: 'align-left'},
+				{type: 'justifycenter', icon: 'align-center'},
+				{type: 'justifyright', icon: 'align-right'},
+				{type: 'justifyfull', icon: 'align-justify'}
+			], 
+			[
+				{toggle: 'hyperlink', icon: 'link'},
+				{type: 'unlink', icon: 'cut'}
+			],
+			[
+				{toggle: 'picture', icon: 'picture'}
+			],
+			[
+				{type: 'undo', icon: 'undo'},
+				{type: 'redo', icon: 'repeat'}
+			]
+		];
+		$.each(toolbarButtons, function(index, list){
+			self.createGroupedTools(list);
+		});
+	},
+
+	createGroupedTools : function(buttons) {
+		var group = dojo.create('div', { 'class' : 'btn-group' });
+		var self = this;
+		$.each(buttons, function(index, type){
+			if(type.toggle == 'font'){
+				self.createFonts(type, group);
+			} else if(type.toggle == 'fontsize'){
+				//this is the dropdown menu rendering for the font size
+				self.createFontSize(type, group);
+			} else if (type.toggle == 'hyperlink'){
+				//Create Linkfield
+				self.createHyperlink(type, group);
+			} else if(type.toggle == 'picture') {
+				self.createPicture(type, group);
+			} else {
+				//Regular button rendering
+				var button = dojo.create('a', { 'class' : 'btn', 'data-edit' : type.type });
+				var icon = dojo.create('i', { 'class' : 'icon-' + type.icon });
+				dojo.place(icon, button);
+				dojo.place(button, group, 'last');
+			}
+		});
+
+
+		dojo.place(group, this._toolbarNode);
+	},
+
+	createFonts : function(type, group){
+		var button = dojo.create('a', { 'class' : 'btn dropdown-toggle', 'title' : '', 'data-toggle' : 'dropdown' });
+		var icon = dojo.create('i', { 'class' : 'icon-' + type.icon });
+		var caret  = dojo.create('b', {'class' : 'caret'});
+		dojo.place(icon, button);
+		dojo.place(caret, button);
+
+		var ul = dojo.create('ul', {'class' : 'dropdown-menu'});
+		var self = this;
+		$.each(type.fonts, function(index, font){
+			var item = dojo.create('li');
+			var fontTag = dojo.create('a', {'data-edit' : 'fontName ' + font.name});
+			dojo.html.set(fontTag, font.name);
+			dojo.place(fontTag, item);
+			dojo.place(item, ul);
+			dojo.style(fontTag, {
+				'fontFamily' : font.name
+			});
+		});
+		dojo.place(button, group, 'last');
+		dojo.place(ul, group, 'last');
+	},
+
+	createFontSize : function(type, group) {
+		var button = dojo.create('a', { 'class' : 'btn dropdown-toggle', 'title' : '', 'data-toggle' : 'dropdown' });
+		var icon = dojo.create('i', { 'class' : 'icon-' + type.icon });
+		var caret  = dojo.create('b', {'class' : 'caret'});
+		dojo.place(icon, button);
+		dojo.place(caret, button);
+
+		var ul = dojo.create('ul', {'class' : 'dropdown-menu'});
+		var self = this;
+		$.each(type.sizes, function(index, size){
+			var item = dojo.create('li');
+			var sizetag = dojo.create('a', {'data-edit' : 'formatBlock ' + size.type});
+			dojo.html.set(sizetag, '<'+ size.type +'>' + size.name + '</' + size.type + '>');
+			self.connect(sizetag, 'onclick', function(e){
+				$('#' + self.id + ' .dropdown-menu').hide();
+			});
+			dojo.place(sizetag, item);
+			dojo.place(item, ul);
+		});
+		dojo.place(button, group, 'last');
+		dojo.place(ul, group, 'last');
+	},
+
+	createHyperlink : function(type, group){
+		var button = dojo.create('a', { 'class' : 'btn dropdown-toggle', 'title' : '', 'data-toggle' : 'dropdown', 'data-original-title' : 'hyperlink'});
+		var icon = dojo.create('i', { 'class' : 'icon-' + type.icon });
+		var dropurl = dojo.create('div', {'class' : 'dropdown-menu input-append'});
+		var urlfield = dojo.create('input', {'class' : 'span2', 'type' : 'text', 'data-edit' : 'createLink', 'placeholder' : 'URL'});
+		var urlbutton = dojo.create('button', {'class' : 'btn', 'type' : 'button'}); 
+		dojo.html.set(urlbutton, 'Add');
+		var self = this;
+		self.connect(urlbutton, 'onclick', function(e){
+			$('#' + self.id + ' .dropdown-menu').hide();
+		});
+
+		dojo.place(urlfield, dropurl);
+		dojo.place(urlbutton, dropurl, 'last');
+		
+		dojo.place(icon, button);
+		dojo.place(button, group, 'last');
+		dojo.place(dropurl, group, 'last');
+	},
+
+	createPicture : function(type, group) {
+		var button = dojo.create('a', { 'id': 'pictureBtn', 'class' : 'btn'});
+		var icon = dojo.create('i', { 'class' : 'icon-' + type.icon });
+		var pictureInput = dojo.create('input', {'type' : 'file', 'data-edit' : 'insertImage', 'data-target' : '#pictureBtn', 'data-role' : 'magic-overlay'});
+		dojo.place(icon, button);
+		dojo.place(button, group, 'last');
+		dojo.place(pictureInput, group, 'last');
+		dojo.style(button, {
+			'position'	: 'relative'
+		});
+		dojo.style(pictureInput, {
+			'opacity'	: 0,
+			'position'	: 'absolute',
+			'top'		: '0.199997px', 
+			'left'		: '-0.0000366211px',
+			'width'		: '39px', 
+			'height'	: '30px'
+		});
+	},
+
+	addEditor : function(){
+		dojo.place(this._toolbarNode, this.domNode);
+		dojo.place(this._inputfield, this.domNode, 'last');
+		//force the MX-styles.
+		dojo.addClass(this._inputfield, 'form-control mx-textarea-input mx-textarea-input-noresize');
+		dojo.style(this._inputfield, {
+			'height' : this.boxHeight + 'px'
+		});
+
+		$('#' + this.id + '_editor').wysiwyg({ toolbarSelector: '[data-role=editor-toolbar-'+ this.id +']'} );
+		this.addListeners();
+	},
+
+	addListeners : function() {
+		var self = this;
+
+		//save which element was clicked
+		var target = null;
+		$(document).mousedown(function(e) {
+	        // The latest element clicked
+	        target = $(e.target);
+	    });
+
+	    // when 'target == null' on blur, we know it was not caused by a click
+	    $(document).mouseup(function(e) {
+	        target = null;
+	    });
+
+		$(this._inputfield).on("blur", function(e){
+			//use the last element that was clicked to find out if it was a toolbar click.
+			if(!target || target.closest('.toolbar_' + self.id).length == 0){
+				self.fetchContent();
+			}
+		});
+
+		//Ok, I'm just going to stick to jquery here for traversing the dom. Much easier.
+		$('#'+ this.id +' .dropdown-toggle').on("click", function(e){
+			$(this).parent().find('div').toggle();
+			$(this).parent().find('ul').toggle();
+		});
+
+		//Check if we have to hide the dropdown box.
+		var self = this;
+		$("#" + this.id).on("click", function(e){
+			var isContainer = self.testTarget(e);
+			if(!isContainer){
+				$('#' + this.id + ' .dropdown-menu').hide();
+			}
+		});
+	},
+
+	fetchContent : function() {
+		var text = $(this._inputfield).html();
+		var valueChanged = (this._mxObj.get(this.attribute) !== text);
+		
+		this._mxObj.set(this.attribute, text);
+		
+		if (valueChanged && this.onchangeMF !== "") {
+			this.execMf(this.onchangeMF, this._mxObj.getGuid());
+		}
+	},
+
+	execMf : function(mf, guid){
+		mx.data.action({
+			params: {
+                applyto     : "selection",
+                actionname  : mf,
+                guids : [guid]
+            }
+        });
+	}, 
+
+	testTarget : function (e) {
+		//See if we clicked the same button
+		var isButton = false;
+		$.each($('#'+ this.id +' .dropdown-toggle'), function(index, object){
+			if (!isButton){
+				isButton = $(e.target).parent()[0] == object || $(e.target) == object;
+			}
+		});
+		//See if we clicked inside the box
+		var isContainer = $(e.target).closest('ul').siblings('.dropdown-toggle').length > 0 ||
+			$(e.target).siblings('.dropdown-toggle').length > 0 ||
+			$(e.target).parent().siblings('.dropdown-toggle').length > 0;
+
+		var value = isContainer;
+		if(isButton === true) {
+			value = isButton;
+		}
+
+		return value;
+	}
+
+});
+
+})(BootstrapRTE.lib.$);
